fa19cc560116edb32230f9797f79cc215aa20fed
Marcel Planas <marcel@iestrategic.com>
2021-12-09 13:06:21 +0100

    Finished courseinfo for part2

---
+++ part2/test/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Note from './components/Note'
+
+const App = (props) => {
+  
+  const [notes, setNotes] = useState(props.notes)
+  
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+            <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
